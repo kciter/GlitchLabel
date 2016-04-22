8b062e866d262824ef977c824d07750549421f48
Lee Sun-Hyoup <kciter@naver.com>
2016-04-22 11:33:59 +0900

    Initial Commit

---
+++ GlitchLabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GlitchLabel
+//
+//  Created by LeeSunhyoup on 2016. 4. 22..
+//  Copyright © 2016년 Lee Sun-Hyoup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
