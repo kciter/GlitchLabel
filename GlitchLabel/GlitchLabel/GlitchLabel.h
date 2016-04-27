9e864433e212cc1fbdef3730ea4e0c8e6d76c076
Lee Sun-Hyoup <kciter@naver.com>
2016-04-27 13:54:01 +0900

    * Add Carthage

---
+++ GlitchLabel/GlitchLabel/GlitchLabel.h
@@ -0,0 +1,19 @@
+//
+//  GlitchLabel.h
+//  GlitchLabel
+//
+//  Created by LeeSunhyoup on 2016. 4. 27..
+//  Copyright © 2016년 Lee Sun-Hyoup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GlitchLabel.
+FOUNDATION_EXPORT double GlitchLabelVersionNumber;
+
+//! Project version string for GlitchLabel.
+FOUNDATION_EXPORT const unsigned char GlitchLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GlitchLabel/PublicHeader.h>
+
+
