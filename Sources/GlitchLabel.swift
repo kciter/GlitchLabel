28dc94e4ccf840c655c06d5a68edd8b9b40e1908
Lee Sun-Hyoup <kciter@naver.com>
2016-09-27 17:02:59 +0900

    Swift 3 migration

--- Sources/GlitchLabel.swift
+++ Sources/GlitchLabel.swift
@@ -9,25 +9,25 @@
 import UIKit
 
 @IBDesignable
-public class GlitchLabel: UILabel {
-    @IBInspectable public var amplitudeBase: Double = 2.0
-    @IBInspectable public var amplitudeRange: Double = 1.0
+open class GlitchLabel: UILabel {
+    @IBInspectable open var amplitudeBase: Double = 2.0
+    @IBInspectable open var amplitudeRange: Double = 1.0
     
-    @IBInspectable public var glitchAmplitude: Double = 10.0
-    @IBInspectable public var glitchThreshold: Double = 0.9
+    @IBInspectable open var glitchAmplitude: Double = 10.0
+    @IBInspectable open var glitchThreshold: Double = 0.9
     
-    @IBInspectable public var alphaMin: Double = 0.8
+    @IBInspectable open var alphaMin: Double = 0.8
     
-    @IBInspectable public var glitchEnabled: Bool = true
-    @IBInspectable public var drawScanline: Bool = true
+    @IBInspectable open var glitchEnabled: Bool = true
+    @IBInspectable open var drawScanline: Bool = true
     
-    public var blendMode: CGBlendMode = .Lighten
+    open var blendMode: CGBlendMode = .lighten
     
-    private var channel: Int = 0
-    private var amplitude: Double = 2.5
-    private var phase: Double = 0.9
-    private var phaseStep: Double = 0.05
-    private var globalAlpha: Double = 0.8
+    fileprivate var channel: Int = 0
+    fileprivate var amplitude: Double = 2.5
+    fileprivate var phase: Double = 0.9
+    fileprivate var phaseStep: Double = 0.05
+    fileprivate var globalAlpha: Double = 0.8
     
     override public init(frame: CGRect) {
         super.init(frame: frame)
@@ -39,9 +39,9 @@ public class GlitchLabel: UILabel {
         setTimer()
     }
     
-    override public func drawTextInRect(rect: CGRect) {
+    override open func drawText(in rect: CGRect) {
         if !glitchEnabled {
-            super.drawTextInRect(rect)
+            super.drawText(in: rect)
             return
         }
         
@@ -69,49 +69,49 @@ public class GlitchLabel: UILabel {
             print("ERROR")
         }
         
-        channelsImage?.drawInRect(bounds)
+        channelsImage?.draw(in: bounds)
         
-        if let channelsImage = channelsImage where drawScanline {
-            getScanlineImage(channelsImage).drawInRect(bounds)
+        if let channelsImage = channelsImage , drawScanline {
+            getScanlineImage(channelsImage).draw(in: bounds)
             if floor(random() * 2) > 1 {
-                getScanlineImage(channelsImage).drawInRect(bounds)
+                getScanlineImage(channelsImage).draw(in: bounds)
             }
         }
     }
     
-    private func getChannelsImage(x1: CGFloat, x2: CGFloat, x3: CGFloat) -> UIImage {
+    fileprivate func getChannelsImage(_ x1: CGFloat, x2: CGFloat, x3: CGFloat) -> UIImage {
         let redImage = getRedImage(bounds)
         let greenImage = getGreenImage(bounds)
         let blueImage = getBlueImage(bounds)
         
-        UIGraphicsBeginImageContextWithOptions(bounds.size, false, UIScreen.mainScreen().scale)
-        redImage.drawInRect(bounds + CGRectMake(x1, 0, 0, 0),
+        UIGraphicsBeginImageContextWithOptions(bounds.size, false, UIScreen.main.scale)
+        redImage.draw(in: bounds + CGRect(x: x1, y: 0, width: 0, height: 0),
                             blendMode: blendMode,
                             alpha: CGFloat(globalAlpha))
-        greenImage.drawInRect(bounds + CGRectMake(x2, 0, 0, 0),
+        greenImage.draw(in: bounds + CGRect(x: x2, y: 0, width: 0, height: 0),
                               blendMode: blendMode,
                               alpha: CGFloat(globalAlpha))
-        blueImage.drawInRect(bounds + CGRectMake(x3, 0, 0, 0),
+        blueImage.draw(in: bounds + CGRect(x: x3, y: 0, width: 0, height: 0),
                              blendMode: blendMode,
                              alpha: CGFloat(globalAlpha))
         let channelsImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
-        return channelsImage
+        return channelsImage!
     }
     
-    private func getScanlineImage(channelsImage: UIImage) -> UIImage {
+    fileprivate func getScanlineImage(_ channelsImage: UIImage) -> UIImage {
         let y = bounds.size.height * CGFloat(random())
         let y2 = bounds.size.height * CGFloat(random())
         
-        UIGraphicsBeginImageContextWithOptions(bounds.size, false, UIScreen.mainScreen().scale)
+        UIGraphicsBeginImageContextWithOptions(bounds.size, false, UIScreen.main.scale)
         let context = UIGraphicsGetCurrentContext()
         
         
-        let provider: CGDataProviderRef = CGImageGetDataProvider(channelsImage.CGImage)!
-        let data: NSData = CGDataProviderCopyData(provider)!
-        let bytes = data.bytes
-        let bytePointer = UnsafePointer<UInt8>(bytes)
+        let provider: CGDataProvider = channelsImage.cgImage!.dataProvider!
+        let data: Data = provider.data! as Data
+        let bytes = (data as NSData).bytes
+        let bytePointer = bytes.assumingMemoryBound(to: UInt8.self)
         
         for col in 0 ..< Int(bounds.size.width) {
             let offset = 4*(Int(y) * Int(bounds.size.width) + col)
@@ -119,55 +119,55 @@ public class GlitchLabel: UILabel {
             let red = bytePointer[offset+1]
             let green = bytePointer[offset+2]
             let blue = bytePointer[offset+3]
-            CGContextSetRGBFillColor(context, CGFloat(red), CGFloat(green), CGFloat(blue), CGFloat(alpha))
-            CGContextFillRect(context, CGRectMake(CGFloat(col), y2, 1, 0.5))
+            context?.setFillColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: CGFloat(alpha))
+            context?.fill(CGRect(x: CGFloat(col), y: y2, width: 1, height: 0.5))
         }
         
         let scanlineImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
-        return scanlineImage
+        return scanlineImage!
     }
     
-    private func getRedImage(rect: CGRect) -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(rect.size, false, UIScreen.mainScreen().scale)
-        text?.drawInRect(rect, withAttributes: [
+    fileprivate func getRedImage(_ rect: CGRect) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, UIScreen.main.scale)
+        text?.draw(in: rect, withAttributes: [
             NSFontAttributeName: UIFont.init(name: font.fontName, size: font.pointSize)!,
-            NSForegroundColorAttributeName: UIColor.redColor()
+            NSForegroundColorAttributeName: UIColor.red
             ])
         
-        let redImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        let redImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         
         return redImage
     }
     
-    private func getGreenImage(rect: CGRect) -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(rect.size, false, UIScreen.mainScreen().scale)
-        text?.drawInRect(rect, withAttributes: [
+    fileprivate func getGreenImage(_ rect: CGRect) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, UIScreen.main.scale)
+        text?.draw(in: rect, withAttributes: [
             NSFontAttributeName: UIFont.init(name: font.fontName, size: font.pointSize)!,
-            NSForegroundColorAttributeName: UIColor.greenColor()
+            NSForegroundColorAttributeName: UIColor.green
             ])
-        let greenImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        let greenImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         
         return greenImage
     }
     
-    private func getBlueImage(rect: CGRect) -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(rect.size, false, UIScreen.mainScreen().scale)
-        text?.drawInRect(rect, withAttributes: [
+    fileprivate func getBlueImage(_ rect: CGRect) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, UIScreen.main.scale)
+        text?.draw(in: rect, withAttributes: [
             NSFontAttributeName: UIFont.init(name: font.fontName, size: font.pointSize)!,
-            NSForegroundColorAttributeName: UIColor.blueColor()
+            NSForegroundColorAttributeName: UIColor.blue
             ])
         
-        let blueImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        let blueImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         
         return blueImage
     }
     
-    @objc private func tick() {
+    @objc fileprivate func tick() {
         phase += phaseStep
         if phase > 1 {
             phase = 0
@@ -178,28 +178,28 @@ public class GlitchLabel: UILabel {
         setNeedsDisplay()
     }
     
-    private func setTimer() {
-        let timer = NSTimer(timeInterval: 1/30.0,
+    fileprivate func setTimer() {
+        let timer = Timer(timeInterval: 1/30.0,
                            target: self,
                            selector: #selector(GlitchLabel.tick),
                            userInfo: nil,
                            repeats: true)
         
-        NSRunLoop.currentRunLoop().addTimer(timer, forMode: NSDefaultRunLoopMode)
+        RunLoop.current.add(timer, forMode: RunLoopMode.defaultRunLoopMode)
     }
     
-    private func random() -> Double {
+    fileprivate func random() -> Double {
         return (Double(arc4random()) / Double(UINT32_MAX))
     }
 }
 
 func +(rect1: CGRect, rect2: CGRect) -> CGRect {
-    return CGRectMake(rect1.origin.x + rect2.origin.x,
-                      rect1.origin.y + rect2.origin.y,
-                      rect1.size.width + rect2.size.width,
-                      rect1.size.height + rect2.size.height)
+    return CGRect(x: rect1.origin.x + rect2.origin.x,
+                      y: rect1.origin.y + rect2.origin.y,
+                      width: rect1.size.width + rect2.size.width,
+                      height: rect1.size.height + rect2.size.height)
 }
 func +(size1: CGSize, size2: CGSize) -> CGSize {
-    return CGSizeMake(size1.width + size2.width,
-                      size1.height + size2.height)
+    return CGSize(width: size1.width + size2.width,
+                      height: size1.height + size2.height)
 }
